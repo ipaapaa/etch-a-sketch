143367da55af81508240f0133f0b803609ec2645
ipaapaa <marc.olata123@gmail.com>
2024-09-25 22:21:22 +0800

    .container now contains 16 x 16 div elements
    must style gridBox class and container class
    must format container properly to achieve grid
    layout for each gridBox element

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+function generateGrid(){
+    const container = document.querySelector(".container");
+    
+    
+
+    const boxPerRow = 16;
+    const numberOfRows = 16;
+
+    for (let i = 0; i < numberOfRows; i++ ){
+        for (let j = 0; j < boxPerRow; j++){
+            const gridBox = document.createElement("div");
+            gridBox.className = "gridbox";
+            container.appendChild(gridBox);
+            
+        }
+       
+
+    }
+}
+
+
+generateGrid();
+
+})
