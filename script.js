ba89ef5d473a27b4896bf3242f530435ead3698f
ipaapaa <marc.olata123@gmail.com>
2024-09-26 21:24:35 +0800

    Implemented feature to toggle drawing using space bar
    must implement feature to clear sketch

--- script.js
+++ script.js
@@ -1,49 +1,68 @@
-
-
-function generateGrid(){
-    const container = document.querySelector(".container");
-    
-    
-
-    const boxPerRow = 16;
-    const numberOfRows = 16;
-
-    for (let i = 0; i < numberOfRows; i++ ){
-        for (let j = 0; j < boxPerRow; j++){
-            const gridBox = document.createElement("div");
-            gridBox.className = "gridbox";
-            container.appendChild(gridBox);
-            
-        }
-       
-
-    }
-}
-
-function changeColor(){
-    const gridBox = document.querySelectorAll(".gridbox");
-    gridBox.forEach((box) => {
-        box.addEventListener("mouseover", () => {
-            box.style.backgroundColor = "#183419";
-        })
-    })
-}
-
-function startDrawing(){
-    document.addEventListener("keydown", (event) => {
-        if (event.key === " " || event.key === "Space"){
-            changeColor();
-        }
-    });
-
-
-    
-}
-
 document.addEventListener("DOMContentLoaded", function() {
+    let flagIsDrawing = false;
+    
+
+    function generateGrid(){
+        const container = document.querySelector(".container");
+        
+        const boxPerRow = 16;
+        const numberOfRows = 16;
+        
+        for (let i = 0; i < numberOfRows; i++ ){
+            for (let j = 0; j < boxPerRow; j++){
+                const gridBox = document.createElement("div");
+                gridBox.className = "gridbox";
+                container.appendChild(gridBox);
+                
+            }
+        
+
+        }
+    }
+
+    function handleMouseOver(event){
+
+        if (flagIsDrawing) {
+            event.target.style.backgroundColor = "#183419";
+        }
+    }
+
+    function changeColor(){
+        boolIsDrawing = true;
+        
+        const gridBox = document.querySelectorAll(".gridbox");
+        gridBox.forEach((box) => {
+            box.addEventListener("mouseover", handleMouseOver)
+         
+            
+        })
+    }
+
+    function stopChangeColor(){
+
+        const gridBox = document.querySelectorAll(".gridbox");
+        gridBox.forEach((box) => {
+            box.removeEventListener("mouseover", handleMouseOver);
+        });
+
+        
+    }
+
+    function toggleDrawing(){
+            document.addEventListener("keydown", (event) => {
+                if (event.key === " " || event.key === "Space") {
+                    flagIsDrawing = !flagIsDrawing;
+                    if (flagIsDrawing) {
+                        changeColor();
+                    } else {
+                        stopChangeColor();
+                    }
+                }
+            });
+}
 
     generateGrid();
-    startDrawing();
-
+    toggleDrawing();
+    
 
 })
